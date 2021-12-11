a9e7718154840907f2b50a66520e129e3e008718
Logan <hello@loganthompson.de>
2021-12-12 00:04:49 +0100

    Day 3

---
+++ Day 3/solution.py
@@ -0,0 +1,93 @@
+import sys
+sys.setrecursionlimit(10000) 
+
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+
+gamma = ''
+delta = ''
+
+length = len(lines[0])
+for i in range(0, length - 1):
+    zero = 0
+    one = 0
+
+    for line in lines:
+        if line[i] == '0':
+            zero += 1
+        else:
+            one += 1
+    
+    if zero > one:
+        gamma += '0'
+    else:
+        gamma += '1'
+
+for c in gamma:
+    if c == '0':
+        delta += '1'
+    else:
+        delta += '0'
+print(gamma)
+print(delta)
+
+solution = int(gamma, 2) * int(delta, 2)
+print(f'Power Consumption: {solution}')
+
+## Part 2 NOT WORKING YET
+def o2(input, start_index):
+    length = len(input)
+    
+    if length == 1:
+        return input[0]
+
+    zero = 0
+    one = 0
+
+    for line in input:
+        if line[start_index] == '0':
+            zero += 1
+        else:
+            one += 1
+        
+    if zero > one:
+        new_input = [x for x in input if x[start_index] != '0']
+        return o2(new_input, start_index + 1)
+
+    else:
+        new_input = [x for x in input if x[start_index] != '1']
+        return o2(new_input, start_index + 1)
+
+def co(input, start_index):
+    length = len(input)
+    
+    if length == 1:
+        return input[0]
+
+    zero = 0
+    one = 0
+
+    for line in input:
+        if line[start_index] == '0':
+            zero += 1
+        else:
+            one += 1
+        
+    if zero < one:
+        new_input = [x for x in input if x[start_index] != '0']
+        return co(new_input, start_index + 1)
+
+    else:
+        new_input = [x for x in input if x[start_index] != '1']
+        return co(new_input, start_index + 1)
+
+
+    
+ox_rate = int(o2(lines, 0), 2)
+co_rate = int(co(lines, 0), 2)
+
+solution = ox_rate * co_rate
+print(f'Life Support Rating: {solution}')
+
