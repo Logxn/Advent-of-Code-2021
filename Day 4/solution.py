e8f6f26cb57568ceaa32d507a0279600ccb0842a
Logan <hello@loganthompson.de>
2021-12-16 23:21:07 +0100

    Update Day 4 Part 1

--- Day 4/solution.py
+++ Day 4/solution.py
@@ -1,5 +1,4 @@
 import re
-import numpy as np
 
 #https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
 def chunks(l, n):
@@ -15,15 +14,14 @@ numbers = lines[0].split(',')
 # We dont want empty lines
 # We dont want random white spaces
 # We dont want the random numbers to appear in list
-lines.remove(lines[0])
-lines = [x for x in lines if len(x) > 2]
-lines = [re.sub('\n', '', x) for x in lines]
-lines = [re.sub(' +', ' ', x) for x in lines]
-lines = [x.strip() for x in lines]
-numbers = [re.sub('\n', '', x) for x in numbers]
+lines.remove(lines[0]) # Remove number draw line
+lines = [x for x in lines if len(x) > 2] # Dirty \n removal
+lines = [re.sub('\n', '', x) for x in lines] # Remove \n at end of row
+lines = [re.sub(' +', ' ', x) for x in lines] # Remove multiple white spaces
+lines = [x.strip() for x in lines] # Remove white space at the beginning
+numbers = [re.sub('\n', '', x) for x in numbers] # Remove \n for the last number drawn
 
-
-wizzard = []
+# Transform board array in a 5 chunk array (5x5 board)
 sliced_arrs = list(chunks(lines, 5))
 
 def get_winning_board():
@@ -42,19 +40,24 @@ def get_winning_board():
                         board[row_index] = row
                         sliced_arrs[board_index] = board
             
-                # Win Check
+                # Win Check (Row-Check)
                 if row == 'x x x x x':
                     remaining_numbers = []
                     for x in board:
                         for y in x.split():
                             if y != 'x':
                                 remaining_numbers.append(y)
+                    # Sum up the remaining numbers and multiply it with the last drawn number
                     solution = sum(map(int, remaining_numbers)) * int(drawn_number)
                     solutions.append(solution)
                     finished_boards.append(board_index)
                     break
-        if drawn_index == len(numbers) - 1:
-            return solutions
+                
+                # Not sure if this works lol
+                if row_index == 0:
+                    for col_index, num in enumerate(row.split()):
+                        if row[col_index] and board[row_index + 1][col_index] and board[row_index + 2][col_index] and board[row_index + 3][col_index] and board[row_index + 4][col_index] == 'x':
+                             print(f'{board_index} Bingo')
 
 solution = get_winning_board()
 print(solution)
